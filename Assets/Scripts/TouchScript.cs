b12a5460e397818739abfcfd06eb96bda92b775c
Danilo de Lucas <daniloluca@live.com>
2015-11-13 01:21:48 -0200

    Adicionado projeto no repositorio

---
+++ Assets/Scripts/TouchScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchScript : MonoBehaviour {
+
+	private Vector2 screenPoint;
+    private Vector2 offset;
+     
+    void OnMouseDown(){
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        transform.GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    void OnMouseUp(){
+        transform.GetComponent<Rigidbody2D>().isKinematic = false;
+    }
+     
+    void OnMouseDrag(){
+        Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        transform.position = curPosition;
+    }
+}
